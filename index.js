56fb6639496675d8718386442b0557dbd7b11fbc
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-11-14 12:13:44 +0530

    Use routes & helper

--- index.js
+++ index.js
@@ -2,23 +2,24 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const path = require('path')
-
+const route = require('./routes')
+const helper = require('./helper')
 app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended: false
 }))
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')))
-app.post('/', (req, res) => {
+app.get(route.home, (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')))
+app.post(route.home, (req, res) => {
   res.send('POST request to the homepage')
   console.log(req.body)
 })
 
 
-app.get('/createTask', (req, res) => res.sendFile(path.join(__dirname, '/public/task.html')))
-app.post('/createTask', (req, res) => {
-  console.log(Object.keys(req.body))
+app.get(route.createTask, (req, res) => res.sendFile(path.join(__dirname, '/public/task.html')))
+app.post(route.createTask, (req, res) => {
+  console.log(helper.sanitize(req.body))
   res.send('creating task!')
 })
 
