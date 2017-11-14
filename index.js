cbc6cd56a52bf22f753d9bb3822f69534cb3f0e7
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-11-14 11:49:33 +0530

    Log object keys

--- index.js
+++ index.js
@@ -4,29 +4,21 @@ const bodyParser = require('body-parser')
 const path = require('path')
 
 app.use(express.static('public'))
-
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')))
-
 app.use(bodyParser.json())
-
 app.use(bodyParser.urlencoded({
   extended: false
 }))
 
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')))
 app.post('/', (req, res) => {
   res.send('POST request to the homepage')
   console.log(req.body)
 })
 
-app.get('/createTask', (req, res) => res.sendFile(path.join(__dirname, '/public/task.html')))
 
+app.get('/createTask', (req, res) => res.sendFile(path.join(__dirname, '/public/task.html')))
 app.post('/createTask', (req, res) => {
-  // console.log(req.body, '\n-------------------------------')
-  // console.log(Object.keys(req.body))
-  let taskInfo = JSON.parse(req.body)
-  console.log(Object.keys(taskInfo))
-  console.log(taskInfo.task_name)
-  // console.log(req.body)
+  console.log(Object.keys(req.body))
   res.send('creating task!')
 })
 
