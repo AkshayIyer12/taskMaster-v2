a3d3d81431fafa55100a4a8b11b7a2427d841a71
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-11-15 18:58:50 +0530

    Add api for create task

--- index.js
+++ index.js
@@ -1,53 +1,76 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const bodyParser = require('body-parser')
-const route = require('./routes')
-const db = require('./db')
-const redis = require('redis')
-const client = redis.createClient()
+ const express = require('express')
+ const cors = require('cors')
+ const app = express()
+ const bodyParser = require('body-parser')
+ const multer = require('multer')
+ const route = require('./routes')
+ const db = require('./db')
+ const redis = require('redis')
+ const client = redis.createClient()
+ let upload = multer()
 
-client.on('error', (err) => console.log('Error ' + err))
-app.use(cors())
-app.use(express.static('public'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-  extended: false
-}))
+ client.on('error', (err) => console.log('Error ' + err))
 
-app.get(route.displayTasks, (req, res) => {
-  let tasks = {
-    'status': 'success',
-    'data': {}
-  }
-  db.getAllTasks(function (err, data) {
-    if (err) {
-      console.log(err)
-    } else {
-      tasks.data = data
-      res.json(tasks)
-    }
-  })
-})
+ app.use(cors())
+ app.use(express.static('public'))
+ app.use(bodyParser.json())
+ app.use(bodyParser.urlencoded({
+   extended: false
+ }))
 
-app.get(route.task, (req, res) => {
-  db.getTaskById(req.params.taskId, (err, value) => {
-    if (err) res.json({'status': 'error', 'message': err.message})
-    else res.json({
-      'status': 'success',
-      'data': value})
-  })
-})
+ app.get(route.displayTasks, (req, res) => {
+   let tasks = {
+     'status': 'success',
+     'data': {}
+   }
+   db.getAllTasks(function (err, data) {
+     if (err) {
+       console.log(err)
+     } else {
+       tasks.data = data
+       res.json(tasks)
+     }
+   })
+ })
 
-app.delete(route.task, (req, res) => {
-  db.deleteTaskById(req.params.taskId, (err, value) => {
-    if (err) res.json({'status': 'error', 'message': err.message})
-    else res.json({
-      'status': 'success',
-      'data': value
-    })
-  })
-})
-app.listen(3000, () => {
-  console.log('running on 3000')
-})
+ app.get(route.task, (req, res) => {
+   db.getTaskById(req.params.taskId, (err, value) => {
+     if (err) {
+       res.json({'status': 'error', 'message': err.message})
+     } else {
+       res.json({
+         'status': 'success',
+         'data': value})
+     }
+   })
+ })
+
+ app.delete(route.task, (req, res) => {
+   db.deleteTaskById(req.params.taskId, (err, value) => {
+     if (err) {
+       res.json({'status': 'error', 'message': err.message})
+     } else {
+       res.json({
+         'status': 'success',
+         'data': value
+       })
+     }
+   })
+ })
+
+ app.post(route.createTask, upload.array(), (req, res) => {
+   db.addToDB(req.body, (err, value) => {
+     if (err) {
+       res.json({'status': 'error', 'message': err.message})
+     } else {
+       res.json({
+         'status': 'success',
+         'data': value
+       })
+     }
+   })
+ })
+
+ app.listen(3000, () => {
+   console.log('running on 3000')
+ })
