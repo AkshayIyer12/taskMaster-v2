b3212d5f4ee58151d0927b824026939a788eef54
Anirudh B S <anirudhbs96@gmail.com>
2017-11-15 17:55:31 +0530

    Display all fields of task

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react'
+import React, {
+  Component
+} from 'react'
 import './App.css'
 import axios from 'axios'
 
-class App extends Component {
+class DisplayTasks extends Component {
   constructor(props) {
     super(props)
 
@@ -10,32 +12,99 @@ class App extends Component {
       names: []
     }
   }
-  renderString() {
-    return ('hello world')
-  }
 
   componentDidMount() {
     axios.get(`http://localhost:3000/tasks`)
       .then(res => {
-        if(res.status !== 'success'){
+        if (res.data.status !== 'success') {
           // handle Error
         }
-
-        const names = res.data.data.map(currentTask => currentTask)
-        this.setState({ names })
+        const names = res.data.data.map(currentTask => {
+          return currentTask
+        })
+        this.setState({
+          names
+        })
       })
   }
-
 
   render() {
     return (
       <div id='allTasks'>
+        <h2>All tasks</h2>
         <ul>
         {this.state.names.map(current =>
-        <li key = {current.taskID}>{current.taskName}</li>
+          <li>{current.taskName} - {current.assignTo} - {current.dueDate} - {current.desc}</li> // key
         )}
         </ul>
       </div>
+    )
+  }
+}
+
+class CreateTask extends Component {
+  constructor(props) {
+    super(props)
+
+this.state = {
+			taskName: '',
+			assignTo: '',
+			dueDate: '',
+			desc:''
+		}
+  }
+
+  render() {
+    return (
+      <div id='createTask'>
+        <h1>createTask</h1>
+            <div>
+              <input 
+              type="text"
+              name="taskName"
+              placeholder="Name"
+              value={ this.state.taskName }
+              onChange={ this.handleChange } 
+              />
+              
+              <input
+              type="text"
+              name="assignTo"
+              placeholder="Assign To"
+              value={ this.state.assignTo }
+              onChange={ this.handleChange }
+              />
+
+              <input
+              type="text"
+              name="dueDate"
+              placeholder="Due Date"
+              value={ this.state.dueDate }
+              onChange={ this.handleChange }
+              />
+
+              <input
+              type="text"
+              name="desc"
+              placeholder="Description"
+              value={ this.state.desc }
+              onChange={ this.handleChange }
+              />
+            
+              <button value="Send" onClick={ this.addTask }>Add</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+    <div>
+      <DisplayTasks />
+      <CreateTask />
+    </div>
     )
   }
 }
