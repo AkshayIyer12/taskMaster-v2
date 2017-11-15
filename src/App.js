6181957f95958539766b09acfb8054f306b28170
Anirudh B S <anirudhbs96@gmail.com>
2017-11-15 13:06:50 +0530

    Add axios dependency for ajax request

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import axios from 'axios'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      posts: []
+    }
+  }
+  renderString() {
+    return ('hello world')
+  }
+
+  componentDidMount() {
+    axios.get(`http://www.reddit.com/r/globalOffensive.json`)
+      .then(res => {
+        const posts = res.data.data.children.map(obj => obj.data)
+        this.setState({ posts })
+      })
+  }
+
   render() {
     return (
-      <h1>hello world</h1>
-    );
+      <div>
+          <ul>
+          {
+            this.state.posts.map(post =>
+            <li key={post.id}>{post.title}</li>
+          )
+          }
+        </ul>
+      </div>
+    )
   }
 }
 
-export default App;
+export default App
