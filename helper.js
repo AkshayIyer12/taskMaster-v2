4d0ca849d222ade78074e98446c2d250a68c8abe
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-11-16 12:18:55 +0530

    Delete helper function

--- helper.js
+++
@@ -1,9 +0,0 @@
-const sanitize = (form) => {
-  let keys = Object.keys(form)
-  let value = keys.filter((k) => form[k] !== '')
-  return keys.length === value.length
-}
-
-module.exports = {
-  sanitize
-}
