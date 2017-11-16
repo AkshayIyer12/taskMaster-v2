c14c69413ca9c14510ee617e3a098ac061e9367c
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-11-17 04:26:51 +0530

    Add updateUser route

--- routes.js
+++ routes.js
@@ -1,11 +1,14 @@
 const routes = {
   displayTasks: '/tasks',
   task: '/task/:taskId',
+  deleteTask: '/task/:taskId',
   createTask: '/task',
   updateTask: '/task/:taskId',
   displayUsers: '/users',
   user: '/user/:userId',
-  createUser: '/user'
+  deleteUser: '/user/:userId',
+  createUser: '/user',
+  updateUser: '/user/:userId'
 }
 
 module.exports = routes
