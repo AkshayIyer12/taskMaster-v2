08f0443b9ddf12c462270b2a2af7894e3ebf51c0
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-11-20 15:50:18 +0530

    Remove comments

--- helper.js
+++ helper.js
@@ -3,15 +3,9 @@ const saltRounds = 10
 
 const hashPassword = (password) => {
   return bcrypt.hash(password, saltRounds)
-  // .then(hash => {
-  //   return hash
-  // })
 }
 const compareHash = (password, hash) => {
   return bcrypt.compare(password, hash)
-  // .then(res => {
-  //   return res
-  // })
 }
 
 module.exports = {
