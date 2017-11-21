94b9134c4ebcd23574bb95a32afbed4d2601f9ca
Anirudh B S <anirudhbs96@gmail.com>
2017-11-21 11:50:33 +0530

    Added cancel button and + button to show form

--- src/TaskForm.js
+++ src/TaskForm.js
@@ -6,6 +6,7 @@ class TaskForm extends Component {
   constructor() {
     super()
     this.state = {
+      showForm: false,
       taskName: '',
       assignTo: '',
       dueDate: '',
@@ -25,9 +26,13 @@ class TaskForm extends Component {
       .then((res) => {
         // handle result
         if (res.data.status === 'success') {
-          console.log('Task added! - ', taskName)
+          console.log('Task added to DB - ', taskName)
           // re-Render allTasks
           this.props.onChange()
+
+          const state = this.state
+          state.showForm = false
+          this.setState(state)
         }
         else {
           // handle error
@@ -38,25 +43,50 @@ class TaskForm extends Component {
 
   onSubmit = (e) => {
     console.log('Submitted')
+    if((this.state.taskName === '') || (this.state.assignTo === '')
+      || (this.state.dueDate === '') || (this.state.desc === '')) {
+      console.log('empty fields!')
+      
+    }
     e.preventDefault()
     // check validity
     this.addTask()
   }
   
+  showForm () {
+    console.log('show form')
+    const state = this.state
+    state.showForm = true
+    this.setState(state)
+  }
+
+  onCancel = (e) => {
+    console.log('Cancelled')
+    const state = this.state
+    state.showForm = false
+    this.setState(state)
+  }
+
   render() {
     const { taskName, assignTo, dueDate, desc } = this.state
-    return (
-      <div>
-        <h2>create task</h2>
-        {/* <form onSubmit={this.onSubmit}> */}
-          <input type='text' name='taskName' value={taskName} onChange={this.onChange} placeholder='Name' />
-          <input type='text' name='assignTo' value={assignTo} onChange={this.onChange} placeholder='Assign to' />
-          <input type='text' name='dueDate' value={dueDate} onChange={this.onChange} placeholder='Due Date' />
-          <input type='textarea' name='desc' value={desc} onChange={this.onChange} placeholder='Description' />
-          <button type='submit' onClick={this.onSubmit}>Submit</button>
-        {/* </form> */}
-      </div>
-    )
+    if(this.state.showForm) {
+      return (
+        <div>
+            <label>Name:<input type='text' name='taskName' value={taskName} onChange={this.onChange} /></label>
+            <label>Assign to:<input type='text' name='assignTo' value={assignTo} onChange={this.onChange} /></label>
+            <label>Due Date:<input type='text' name='dueDate' value={dueDate} onChange={this.onChange} /></label>
+            <label>Description:<input type='textarea' name='desc' value={desc} onChange={this.onChange} /></label>
+            <button onClick={this.onSubmit}>Submit</button>
+            <button onClick={this.onCancel}>Cancel</button>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <button onClick={this.showForm.bind(this)}>+</button>
+        </div>
+      )
+    }
   }
 }
 
