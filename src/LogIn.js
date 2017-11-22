653079b1e50b7558061aab09a4d8380c651fa339
Anirudh B S <anirudhbs96@gmail.com>
2017-11-22 16:02:04 +0530

    Implemented userID as state of LogIn Component

--- src/LogIn.js
+++ src/LogIn.js
@@ -3,8 +3,33 @@ import axios from 'axios'
 
 class LogIn extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+        id: ''
+      }
+    }
+
+  componentDidMount() {
+    console.log('mounted login')
+    const cookieList = document.cookie.split(';')
+    let id = ''
+    for(let i of cookieList) {
+      if(i.startsWith(' userId')) id = i.slice(15, 39).trim()
+    }
+    const state = this.state
+    state.id = id
+    this.setState(state)
+    
+    console.log(this.state.id)
+    
+    // while(true){
+    //   // window.open('http://localhost:3001/tasks')
+    // }
+  }
+  
   openGoogle() {
-    window.open('http://localhost:3000/auth/google', '_self')
+    window.open('http://localhost:3000/auth/google', '')
   }
   
   logOut () {
