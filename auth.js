dd3b2c4a9c0fb49ebf52f50028032ad7cb468b57
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-11-23 15:20:10 +0530

    Hide important stuff

--- auth.js
+++ auth.js
@@ -1,17 +1,16 @@
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
-
+const config = require('./config.js')
 module.exports = (passport) => {
   passport.serializeUser((user, done) => {
     done(null, {})
   })
   passport.deserializeUser((user, done) => {
-    console.log('Deserialize ' + user)
     done(null, user)
   })
   passport.use(new GoogleStrategy({
-    clientID: '628987246742-tmfpj8epv74g6orsnkf20ahpcspvdvs2.apps.googleusercontent.com',
-    clientSecret: 'cdr3DQL1Hbug0UmY226YLwfU',
-    callbackURL: 'http://localhost:3000/auth/google/callback'
+    clientID: config.clientID,
+    clientSecret: config.clientSecret,
+    callbackURL: config.callbackURL
   }, (token, refreshToken, profile, done) => {
     return done(null, {
       profile: profile,
