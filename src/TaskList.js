c13222c5be1b55a67f93ef028c0d84f4d65e4721
Anirudh B S <anirudhbs96@gmail.com>
2017-11-24 11:28:19 +0530

    Moved both tasklist components to different files

--- src/TaskList.js
+++ src/TaskList.js
@@ -1,58 +1,7 @@
 import axios from 'axios'
 import React, { Component } from 'react'
-import IndividualTask from './IndividualTask'
-
-class Tasks extends Component {
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      taskList: []
-    }
-  }
-
-  render() {
-    return (
-      <div id='allTasks'>
-        <h2>assigned to you</h2>
-        <ul>
-          {
-            this.props.value.map(current =>
-              <IndividualTask key={current._id} currentTask={current}
-                onChange={this.props.onChange} getDetails={this.props.getDetails} />
-            )
-          }
-        </ul>
-      </div>
-    )
-  }
-}
-
-class TasksTwo extends Component {
-  
-  constructor(props) {
-    super(props)
-    this.state = {
-      taskList: []
-    }
-  }
-
-  render() {
-    return (
-      <div id='allTasks'>
-        <h2>assigned by you</h2>
-        <ul>
-          {
-            this.props.value.map(current =>
-              <IndividualTask key={current._id} currentTask={current}
-                onChange={this.props.onChange} getDetails={this.props.getDetails} />
-            )
-          }
-        </ul>
-      </div>
-    )
-  }
-}
+import  TasksForYou from './TasksForYou'
+import TasksByYou from './TasksByYou'
 
 class TaskList extends Component {
 
@@ -106,8 +55,8 @@ class TaskList extends Component {
       <div>
         <button onClick={this.addNewTask.bind(this)}>Add Task</button>
         {/* <button className="waves-effect waves-light btn-large" onClick={this.addNewTask.bind(this)}>Add Task</button> */}
-        <Tasks value={this.state.taskList} onChange={this.onChange.bind(this)} getDetails={this.getDetails.bind(this)}/>
-        <TasksTwo value={this.state.taskList2 } onChange={this.onChange.bind(this)} getDetails={this.getDetails.bind(this)}/>
+        <TasksForYou value={this.state.taskList} onChange={this.onChange.bind(this)} getDetails={this.getDetails.bind(this)}/>
+        <TasksByYou value={this.state.taskList2 } onChange={this.onChange.bind(this)} getDetails={this.getDetails.bind(this)}/>
       </div>
     )
   }
---
+++ src/TasksForYou.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import IndividualTask from './IndividualTask'
+
+class TasksForYou extends Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      taskList: []
+    }
+  }
+
+  render() {
+    return (
+      <div id='allTasks'>
+        <h2>assigned to you</h2>
+        <ul>
+          <li key={this.props.id} className='task-list'>
+            <span className='field task-name'><b>Task Name</b></span>
+            <span className='field assign-to'><b>Assigned To</b></span>
+            <span className='field due-date'><b>Due Date</b></span>
+            <span className='field desc'><b>Description</b></span>
+            <span className='button large material-icons'><i>menu</i></span>
+          </li>
+          {
+            this.props.value.map(current =>
+              <IndividualTask key={current._id} currentTask={current}
+                onChange={this.props.onChange} getDetails={this.props.getDetails} />
+            )
+          }
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default TasksForYou
